6fc63c8fd442e6b5e2f80982bee38a4ccb99b6b9
Ken Youens-Clark <kyclark@gmail.com>
2024-07-15 16:13:47 -0700

    some ideas

--- src/distance_functions.rs
+++ src/distance_functions.rs
@@ -1,57 +1,84 @@
-use anyhow::{bail, Result};
+use anyhow::Result;
 use seahash::SeaHasher;
-use std::hash::{Hash, Hasher};
 use std::collections::HashMap;
+use std::hash::{Hash, Hasher};
 
+use crate::DistanceFunction;
 
+// --------------------------------------------------
 pub fn match_distance_function(
     base_item_bag: Vec<&[char]>,
     mod_item_bag: Vec<&[char]>,
-    distance_function: &str
+    distance_function: DistanceFunction,
 ) -> Result<f64> {
     match distance_function {
-        "euclidean_distance" => euclidean_distance(base_item_bag, mod_item_bag),
-        "cosine_similarity" => cosine_similarity(base_item_bag, mod_item_bag),
-        "jaccard_similarity" => jaccard_similarity(base_item_bag, mod_item_bag),
-        "minhash" => minhash_similarity(base_item_bag, mod_item_bag),
-        _ => {bail!("Unknown distance function: {}", distance_function);}
+        DistanceFunction::EuclideanDistance => {
+            euclidean_distance(base_item_bag, mod_item_bag)
+        }
+        DistanceFunction::CosineSimilarity => {
+            cosine_similarity(base_item_bag, mod_item_bag)
+        }
+        DistanceFunction::JaccardSimilarity => {
+            jaccard_similarity(base_item_bag, mod_item_bag)
+        }
+        DistanceFunction::MinHash => {
+            minhash_similarity(base_item_bag, mod_item_bag)
+        }
     }
 }
 
-pub fn frequency_vectors<'a>(base_item_bag: Vec<&'a [char]>, mod_item_bag: Vec<&'a [char]>
+// --------------------------------------------------
+pub fn frequency_vectors<'a>(
+    base_item_bag: Vec<&'a [char]>,
+    mod_item_bag: Vec<&'a [char]>,
 ) -> (HashMap<&'a [char], f64>, HashMap<&'a [char], f64>) {
     let mut base_item_counts: HashMap<&[char], f64> = HashMap::new();
     let mut mod_item_counts: HashMap<&[char], f64> = HashMap::new();
     for item in base_item_bag {
-        base_item_counts.entry(item).and_modify(|count| *count += 1.0).or_insert(1.0);
+        base_item_counts
+            .entry(item)
+            .and_modify(|count| *count += 1.0)
+            .or_insert(1.0);
         mod_item_counts.entry(item).or_insert(0.0);
     }
     for item in mod_item_bag {
-        mod_item_counts.entry(item).and_modify(|count| *count += 1.0).or_insert(1.0);
+        mod_item_counts
+            .entry(item)
+            .and_modify(|count| *count += 1.0)
+            .or_insert(1.0);
         base_item_counts.entry(item).or_insert(0.0);
     }
     (base_item_counts, mod_item_counts)
 }
 
-pub fn euclidean_distance(base_item_bag: Vec<&[char]>, mod_item_bag: Vec<&[char]>,
+// --------------------------------------------------
+pub fn euclidean_distance(
+    base_item_bag: Vec<&[char]>,
+    mod_item_bag: Vec<&[char]>,
 ) -> Result<f64> {
-    let (base_item_counts, mod_item_counts) = frequency_vectors(base_item_bag, mod_item_bag);
+    let (base_item_counts, mod_item_counts) =
+        frequency_vectors(base_item_bag, mod_item_bag);
 
     let mut distance = 0.0;
     for (item, base_count) in base_item_counts.iter() {
         let mod_count = mod_item_counts.get(item).unwrap_or(&0.0);
-        distance += (base_count.max(*mod_count) - base_count.min(*mod_count)).powf(2.0);
+        distance += (base_count.max(*mod_count) - base_count.min(*mod_count))
+            .powf(2.0);
     }
     Ok(distance.sqrt())
 }
 
-/* This function calculates the cosine similarity between two strings' kmeer vectors.
- * cosine similarity = (v1 * v2) / (||v1|| * ||v2||)
+// --------------------------------------------------
+/* This function calculates the cosine similarity between two strings' kmeer
+ * vectors.  cosine similarity = (v1 * v2) / (||v1|| * ||v2||)
  */
-pub fn cosine_similarity(base_item_bag: Vec<&[char]>, mod_item_bag: Vec<&[char]>
+pub fn cosine_similarity(
+    base_item_bag: Vec<&[char]>,
+    mod_item_bag: Vec<&[char]>,
 ) -> Result<f64> {
-    let (base_item_counts, mod_item_counts) = frequency_vectors(base_item_bag, mod_item_bag);
-    
+    let (base_item_counts, mod_item_counts) =
+        frequency_vectors(base_item_bag, mod_item_bag);
+
     let mut base_magnitude = 0.0;
     let mut mod_magnitude = 0.0;
     let mut dot_product = 0.0;
@@ -67,9 +94,13 @@ pub fn cosine_similarity(base_item_bag: Vec<&[char]>, mod_item_bag: Vec<&[char]>
     Ok(dot_product / (base_magnitude.sqrt() * mod_magnitude.sqrt()))
 }
 
-pub fn jaccard_similarity(base_item_bag: Vec<&[char]>, mod_item_bag: Vec<&[char]>
+// --------------------------------------------------
+pub fn jaccard_similarity(
+    base_item_bag: Vec<&[char]>,
+    mod_item_bag: Vec<&[char]>,
 ) -> Result<f64> {
-    let (base_item_counts, mod_item_counts) = frequency_vectors(base_item_bag, mod_item_bag);
+    let (base_item_counts, mod_item_counts) =
+        frequency_vectors(base_item_bag, mod_item_bag);
 
     let mut intersection = 0.0;
     let mut union = 0.0;
@@ -81,9 +112,12 @@ pub fn jaccard_similarity(base_item_bag: Vec<&[char]>, mod_item_bag: Vec<&[char]
     Ok(intersection / union)
 }
 
-pub fn minhash_similarity(base_item_bag: Vec<&[char]>, mod_item_bag: Vec<&[char]>
+// --------------------------------------------------
+pub fn minhash_similarity(
+    base_item_bag: Vec<&[char]>,
+    mod_item_bag: Vec<&[char]>,
 ) -> Result<f64> {
-    fn hash_function<T: Hash + ?Sized> (item: &T, seed: u64) -> u64 {
+    fn hash_function<T: Hash + ?Sized>(item: &T, seed: u64) -> u64 {
         let mut hasher = SeaHasher::with_seeds(seed, seed, seed, seed);
         item.hash(&mut hasher);
         hasher.finish()
@@ -91,10 +125,16 @@ pub fn minhash_similarity(base_item_bag: Vec<&[char]>, mod_item_bag: Vec<&[char]
     let mut agreement_hash = 0;
     let mut total_hash = 0;
     for i in 0..64 {
-        let base_signature = base_item_bag.iter().map(|item| hash_function(item, i)).min();
-        let mod_signature = mod_item_bag.iter().map(|item| hash_function(item, i)).min();
+        let base_signature = base_item_bag
+            .iter()
+            .map(|item| hash_function(item, i))
+            .min();
+        let mod_signature =
+            mod_item_bag.iter().map(|item| hash_function(item, i)).min();
         total_hash += 1;
-        if base_signature == mod_signature {agreement_hash += 1}
+        if base_signature == mod_signature {
+            agreement_hash += 1
+        }
     }
     Ok(agreement_hash as f64 / total_hash as f64)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,45 @@
+pub mod distance_functions;
+
 use anyhow::{bail, Result};
+use clap::{builder::PossibleValue, ValueEnum};
 use seahash::SeaHasher;
 use std::hash::{Hash, Hasher};
 
-pub mod distance_functions;
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub enum DistanceFunction {
+    EuclideanDistance,
+    CosineSimilarity,
+    JaccardSimilarity,
+    MinHash,
+}
 
+impl ValueEnum for DistanceFunction {
+    fn value_variants<'a>() -> &'a [Self] {
+        &[
+            DistanceFunction::EuclideanDistance,
+            DistanceFunction::CosineSimilarity,
+            DistanceFunction::JaccardSimilarity,
+            DistanceFunction::MinHash,
+        ]
+    }
+
+    fn to_possible_value<'a>(&self) -> Option<PossibleValue> {
+        Some(match self {
+            DistanceFunction::EuclideanDistance => PossibleValue::new("euc"),
+            DistanceFunction::CosineSimilarity => PossibleValue::new("cos"),
+            DistanceFunction::JaccardSimilarity => PossibleValue::new("jac"),
+            DistanceFunction::MinHash => PossibleValue::new("min"),
+        })
+    }
+}
+
+// --------------------------------------------------
 pub fn kmer_similarity(
     base_seq: &[char],
     mod_sequence: &[char],
-    distance_function: &str,
+    distance_function: DistanceFunction,
     k: usize,
-    step: usize
+    step: usize,
 ) -> Result<f64> {
     let base_kmers = generate_kmers_with_step(base_seq, k, step)?;
     let mod_kmers = generate_kmers_with_step(mod_sequence, k, step)?;
@@ -17,28 +47,26 @@ pub fn kmer_similarity(
     distance_functions::match_distance_function(
         base_kmers,
         mod_kmers,
-        distance_function
+        distance_function,
     )
 }
 
+// --------------------------------------------------
 // This function simply takes a string and returns the set of k-mers.
 pub fn generate_kmers(sequence: &[char], k: usize) -> Result<Vec<&[char]>> {
     generate_kmers_with_step(sequence, k, 1)
 }
 
+// --------------------------------------------------
 pub fn generate_kmers_with_step(
     sequence: &[char],
     k: usize,
-    step: usize
+    step: usize,
 ) -> Result<Vec<&[char]>> {
     let mut kmers = vec![];
 
     if k > sequence.len() {
-        bail!(
-            "K ({}) is less than string length {}",
-            k,
-            sequence.len()
-        );
+        bail!("K ({}) is less than string length {}", k, sequence.len());
     }
 
     for i in (0..=(sequence.len() - k)).step_by(step) {
@@ -47,15 +75,15 @@ pub fn generate_kmers_with_step(
     Ok(kmers)
 }
 
-/* This function calculates the euclidean distance between kmer-vector representations
-* of minimizer representations of strings. */
+/* This function calculates the euclidean distance between kmer-vector
+ * representations of minimizer representations of strings. */
 pub fn minimizer_similarity(
     base_seq: &[char],
     mod_seq: &[char],
-    distance_function: &str,
+    distance_function: DistanceFunction,
     k: usize,
     w: usize,
-    step: usize
+    step: usize,
 ) -> Result<f64> {
     let base_minimizers = generate_minimizers(base_seq, k, w, step)?;
     let mod_minimizers = generate_minimizers(mod_seq, k, w, step)?;
@@ -63,21 +91,25 @@ pub fn minimizer_similarity(
     distance_functions::match_distance_function(
         base_minimizers,
         mod_minimizers,
-        distance_function
+        distance_function,
     )
 }
 
-/* This function generates a set of minimizers from a string. It uses my_hash_function
- * with a set seed (69). It only uses ONE hash function.
+/* This function generates a set of minimizers from a string. It uses
+ * my_hash_function with a set seed (69). It only uses ONE hash function.
  */
 pub fn generate_minimizers(
     seq: &[char],
     k: usize,
     w: usize,
-    step: usize
+    step: usize,
 ) -> Result<Vec<&[char]>> {
-    if k > w {bail!("k ({}) > w ({})", k, w);}
-    if w > seq.len() {bail!("w ({}) > sequence length ({})", w, seq.len());}
+    if k > w {
+        bail!("k ({}) > w ({})", k, w);
+    }
+    if w > seq.len() {
+        bail!("w ({}) > sequence length ({})", w, seq.len());
+    }
 
     let mut minimizers: Vec<&[char]> = Vec::new();
     for idx in (0..seq.len() - w).step_by(step) {
@@ -88,43 +120,48 @@ pub fn generate_minimizers(
     Ok(minimizers)
 }
 
-pub fn generate_single_minimizer(window: &[char], k: usize) -> Result<&[char]> {
+// --------------------------------------------------
+pub fn generate_single_minimizer(
+    window: &[char],
+    k: usize,
+) -> Result<&[char]> {
     let seed: u64 = 69;
     let kmers = generate_kmers(window, k)?;
     let minimizer = kmers
-            .iter()
-            .min_by_key(|item| my_hash_function(item, seed))
-            .unwrap();
+        .iter()
+        .min_by_key(|item| my_hash_function(item, seed))
+        .unwrap();
     Ok(minimizer)
 }
 
 /* This is a simple hash function that maps items (kmers) to u64 integers.
-* It uses a seed, so this is technically a series of hash functions.
+ * It uses a seed, so this is technically a series of hash functions.
 */
-fn my_hash_function<T: Hash> (item: &T, seed: u64) -> u64 {
+fn my_hash_function<T: Hash>(item: &T, seed: u64) -> u64 {
     let mut hasher = SeaHasher::with_seeds(seed, seed, seed, seed);
     item.hash(&mut hasher);
     hasher.finish()
 }
 
-/* This struct manages ownership of string slices. Strobemers are constructed from strobes,
- * each of which is its own borrowed string slice. These slices need to be concatenated
- * into a bigger string slice, so this struct takes ownership so the concatenated string
+/* This struct manages ownership of string slices. Strobemers are
+ * constructed from strobes, each of which is its own borrowed string
+ * slice. These slices need to be concatenated into a bigger string
+ * slice, so this struct takes ownership so the concatenated string
  * slice stays alive while it's being used for calculations.
  */
 struct Strobemer {
-    chars: Vec<char>
+    chars: Vec<char>,
 }
 
 impl Strobemer {
     pub fn new(strobes: Vec<&[char]>) -> Self {
-        let strobemer_length: usize = strobes.iter()
-            .map(|strobe| strobe.len()).sum();
+        let strobemer_length: usize =
+            strobes.iter().map(|strobe| strobe.len()).sum();
         let mut strobemer = Vec::with_capacity(strobemer_length);
         for strobe in strobes {
             strobemer.extend_from_slice(strobe);
         }
-        Strobemer {chars: strobemer}
+        Strobemer { chars: strobemer }
     }
 
     pub fn data_ref(&self) -> &[char] {
@@ -132,41 +169,46 @@ impl Strobemer {
     }
 }
 
-/*  This function generates a string's set of strobemers. Strobemers are quirky,
-so here's an intuitive breakdown of what strobemers are and how they're generated:
-
-* Stromebers are the concatenation of strobes. There are *order* strobes in each
-strobemer.
-* Each strobemer is drawn from a span of length strobemer_span_length =
-            strobe_length + (strobe_window_gap + strobe_window_length) * order,
-representing the first strobe and all of the remaining strobe windows.
-* In any sequence, there are
-            (sequence.len() - strobemer_span_length) / step
-strobemers. 
-* The n'th strobemer is drawn from
-            sequence[n * step..n * step + order * l].
-* In each strobemer there are *order* strobes.
-* In each strobemer, the first strobe's index is alwoys at the very start of the
-strobemer's span. It is NOT determined by the argmin of a hash function.
-* The n'th strobe (not including the first strobe, of course) is the l-mer minimizer
-of a window within the strobemer's span. Specifically, that window is:
-        window_end = strobe_length + (strobe_window_gap + strobe_window_length) * n
-        window_end = window_start - strobe_window_length
-        strobemer_span[window_start..window_end].
-*/
+/// This function generates a string's set of strobemers. Strobemers
+/// are quirky, so here's an intuitive breakdown of what strobemers
+/// are and how they're generated:
+///
+/// Stromebers are the concatenation of strobes. There are *order*
+/// strobes in each strobemer.
+/// Each strobemer is drawn from a span of length strobemer_span_length =
+///    strobe_length + (strobe_window_gap + strobe_window_length) * order,
+/// representing the first strobe and all of the remaining strobe windows.
+/// In any sequence, there are
+///    (sequence.len() - strobemer_span_length) / step
+/// strobemers.
+///
+/// The n'th strobemer is drawn from
+///    sequence[n * step..n * step + order * l].
+/// In each strobemer there are *order* strobes.
+/// In each strobemer, the first strobe's index is alwoys at the very
+/// start of the strobemer's span. It is NOT determined by the argmin
+/// of a hash function.
+/// The n'th strobe (not including the first strobe, of
+/// course) is the l-mer minimizer of a window within the strobemer's
+/// span. Specifically, that window is:
+///    window_end = strobe_length + (strobe_window_gap + strobe_window_length) * n
+///    window_end = window_start - strobe_window_length
+///    strobemer_span[window_start..window_end].
 pub fn strobemer_similarity(
     base_seq: &[char],
     mod_seq: &[char],
-    distance_function: &str,        // distance function to be used (e.g. cosine similarity).
-    order: usize,                   // the number of concatenated strobes in each strobemer.
-    strobe_length: usize,           // the length of each strobe.
-    strobe_window_gap: usize,       // the gap between strobe selection windows.
-    strobe_window_length: usize,    // the size of each strobe's selection window.
-    step: usize                     // the space between strobemer selection windows.
-)-> Result<f64> {
+    distance_function: DistanceFunction,
+    order: usize, // the number of concatenated strobes in each strobemer.
+    strobe_length: usize, // the length of each strobe.
+    strobe_window_gap: usize, // the gap between strobe selection windows.
+    strobe_window_length: usize, // the size of each strobe's selection window.
+    step: usize, // the space between strobemer selection windows.
+) -> Result<f64> {
     if strobe_window_length < strobe_length {
-        bail!("Strobe window length ({}) is smaller than the length of the strobe ({})",
-            strobe_window_length, strobe_length);
+        bail!(
+            "Strobe window length ({strobe_window_length}) is smaller than 
+            the length of the strobe ({strobe_length})",
+        );
     }
 
     let base_strobemers = generate_strobemers(
@@ -175,22 +217,30 @@ pub fn strobemer_similarity(
         strobe_length,
         strobe_window_gap,
         strobe_window_length,
-        step
+        step,
     )?;
     let base_strobemers_chars: Vec<&[char]> = base_strobemers
-        .iter().map(|strobemer| strobemer.data_ref()).collect();
+        .iter()
+        .map(|strobemer| strobemer.data_ref())
+        .collect();
     let mod_strobemers = generate_strobemers(
         mod_seq,
         order,
         strobe_length,
         strobe_window_gap,
         strobe_window_length,
-        step
+        step,
     )?;
     let mod_strobemers_chars: Vec<&[char]> = mod_strobemers
-        .iter().map(|strobemer| strobemer.data_ref()).collect();
+        .iter()
+        .map(|strobemer| strobemer.data_ref())
+        .collect();
 
-    distance_functions::match_distance_function(base_strobemers_chars, mod_strobemers_chars, distance_function)
+    distance_functions::match_distance_function(
+        base_strobemers_chars,
+        mod_strobemers_chars,
+        distance_function,
+    )
 }
 
 /* HELPER FUNCTION FOR strobemer_euclidean_distance()
@@ -202,11 +252,11 @@ fn generate_strobemers(
     strobe_length: usize,
     strobe_window_gap: usize,
     strobe_window_length: usize,
-    step: usize
+    step: usize,
 ) -> Result<Vec<Strobemer>> {
     let mut strobemers: Vec<Strobemer> = Vec::new(); // custom struct to manage slice ownership.
-    let strobemer_span = strobe_length +
-        order * (strobe_window_length + strobe_window_gap);
+    let strobemer_span =
+        strobe_length + order * (strobe_window_length + strobe_window_gap);
     let last_strobemer_start_index = seq.len() - strobemer_span; // try + 1?
 
     for idx in (0..last_strobemer_start_index).step_by(step) {
@@ -216,33 +266,35 @@ fn generate_strobemers(
             order,
             strobe_length,
             strobe_window_gap,
-            strobe_window_length
+            strobe_window_length,
         )?;
         strobemers.push(strobemer);
     }
     Ok(strobemers)
 }
 
-/* This function generates the first strobemer from a designated window. This means the first
- * strobe is always seq[0..=l]. The subsequent strobes are generated by generate_single_minimizer()
- * from the window designated by the strobemer paper.
- *
- * To generate a string's entire set of strobemers, use this function as a HELPER FUNCTION.
- * Pass it a reference to the substring you want to generate a strobemer from, with the first
- * strobemer being seq[0..=l].
- */
+/// This function generates the first strobemer from a designated
+/// window. This means the first strobe is always seq[0..=l]. The
+/// subsequent strobes are generated by generate_single_minimizer()
+/// from the window designated by the strobemer paper.
+///
+/// To generate a string's entire set of strobemers, use this function
+/// as a HELPER FUNCTION.  Pass it a reference to the substring you
+/// want to generate a strobemer from, with the first strobemer being
+/// seq[0..=l].
 fn generate_single_strobemer(
     strobemer_window: &[char],
     order: usize,
     strobe_length: usize,
     strobe_window_gap: usize,
-    strobe_window_length: usize
+    strobe_window_length: usize,
 ) -> Result<Strobemer> {
     let mut strobes: Vec<&[char]> = Vec::new();
     let first_strobe = &strobemer_window[0..strobe_length];
     strobes.push(first_strobe);
     for n in 1..order {
-        let window_end = strobe_length + (strobe_window_gap + strobe_window_length) * n;
+        let window_end =
+            strobe_length + (strobe_window_gap + strobe_window_length) * n;
         let window_start = window_end - strobe_window_length;
         let strobe_window = &strobemer_window[window_start..window_end];
         let strobe = generate_single_minimizer(strobe_window, strobe_length)?;
@@ -263,23 +315,31 @@ pub fn tensor_slide_sketch(base_seq: &[char],
 
 #[cfg(test)]
 mod similarity_method_tests {
-    use pretty_assertions::assert_eq;
-
     use super::strobemer_similarity;
+    use crate::DistanceFunction;
+    use pretty_assertions::assert_eq;
 
     #[test]
     fn strobemer_euclidean_distance_test_1() {
-        let distance_function = "cosine_similarity";
         let order = 2;
         let l = 3;
         let w_min = 6;
-        let w_max  = 12;
+        let w_max = 12;
         let step = (w_max - w_min) / 2;
 
         let seq1: Vec<char> = "ACGTTGCA".chars().collect();
         let seq2 = seq1.clone();
         let expected = 0.0;
-        let res = strobemer_similarity(&seq1, &seq2, distance_function, order, l, w_min, w_max, step);
+        let res = strobemer_similarity(
+            &seq1,
+            &seq2,
+            DistanceFunction::CosineSimilarity,
+            order,
+            l,
+            w_min,
+            w_max,
+            step,
+        );
         assert!(res.is_ok());
         assert_eq!(res.unwrap(), expected);
     }
