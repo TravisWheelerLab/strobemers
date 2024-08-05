0b958af1634b2d8034fef25d737b1eccb4c107a4
Zach Keyes <56801478+imzoc@users.noreply.github.com>
2024-08-05 15:20:39 +0200

    Data preparation script done Full length cDNA sequences are generated with a pychopper pdf.

--- data/sahlin_data/preprocess-pychopper.py
+++ data/sahlin_data/preprocess-pychopper.py
@@ -7,5 +7,6 @@ for f in files:
         ["pychopper",
         f"{subdir}/{f}.fastq",
         f"{subdir}/{f}_full.fastq",
-        "-S", f"{subdir}/{f}_stats.tsv"]
+        "-S", f"{subdir}/{f}_stats.tsv",
+        "-r", f"{subdir}/{f}_pychopper.pdf"]
     )
