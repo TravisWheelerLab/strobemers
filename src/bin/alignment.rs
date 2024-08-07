d3d5329975a1840ba335d7d27368b8f2298c2971
Zach Keyes <56801478+imzoc@users.noreply.github.com>
2024-08-07 22:31:23 +0200

    updated alignment.rs to have no database functionality

--- src/bin/alignment.rs
+++ src/bin/alignment.rs
@@ -5,7 +5,6 @@ use bio::io::fasta;
 use bio::alignment::distance::levenshtein;
 use anyhow::Result;
 use clap::Parser;
-use rusqlite::{self, params};
 
 use alignment_free_methods;
 
@@ -26,12 +25,8 @@ fn main() {
 // --------------------------------------------------
 // See this repo's README file for pseudocode
 fn run(args: StrobemerArgs) -> Result<()> {
-    let seed_name = format!("alignment");
+    let _seed_name = format!("alignment");
     let project_dir = std::env::var("CARGO_MANIFEST_DIR")?;
-    let comparison_db_conn = alignment_free_methods::cli::initialize_comparison_db(
-        Path::new(&project_dir).join("tests/outputs/comparisons.db")
-    )?;
-
     let query_reader = fasta::Reader::from_file(
         Path::new(&project_dir)
             .join("tests/inputs")
@@ -53,15 +48,11 @@ fn run(args: StrobemerArgs) -> Result<()> {
             io::stdout().flush().unwrap();
             
             let start = Instant::now();
-            let estimated_distance = levenshtein(
+            let _estimated_distance = levenshtein(
                 query_record.seq(),
                 reference_record.seq(),
             );
-            let duration = start.elapsed().subsec_millis();
-            comparison_db_conn.execute(
-                "INSERT OR REPLACE INTO comparisons (query_name, reference_name, seed_name, score, time) VALUES (?1, ?2, ?3, ?4, ?5)",
-                params![query_record.id(), reference_record.id(), seed_name, estimated_distance, duration],
-            )?;
+            let _duration = start.elapsed().subsec_millis();
         }
     }
     Ok(())
