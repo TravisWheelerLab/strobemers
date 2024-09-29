177a710516ce1b576080cf1e7f2e8dea37043d1a
Zach Keyes <56801478+imzoc@users.noreply.github.com>
2024-09-28 18:28:49 -0700

    Tons of tests, Ken will be proud

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use cli::*;
-use rand::seq;
 use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 use std::vec;
@@ -11,7 +10,7 @@ pub mod cli;
 // --------------------------------------------------
 // Seed Object (how do we represent a seed?)
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Eq)]
 pub struct SeedObject {
     pub identifier: u64, // seed representation, such as a hash
     pub ref_index: usize, // I have no idea what this is, it's just stored here for reference.
@@ -20,10 +19,11 @@ pub struct SeedObject {
 }
 impl PartialEq for SeedObject {
     fn eq(&self, other: &Self) -> bool {
-        self.identifier == other.identifier
+        (self.identifier == other.identifier) &
+        (self.local_index == other.local_index) &
+        (self.word_indices == other.word_indices)
     }
 }
-impl  Eq for SeedObject{}
 impl core::hash::Hash for SeedObject {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
         self.identifier.hash(state);
@@ -33,19 +33,49 @@ impl core::hash::Hash for SeedObject {
 // --------------------------------------------------
 // Kmer code
 
-// return u2-encoded nt
-// it has to be a u8 because u2 type doesn't exist
+// A table to map utf-8 encoded characters to our custom u2 encoding
+// for nucleotides.
+//
+// "A" and "a" map to 0 or 0b00
+// "C" and "c" map to 1 or 0b01
+// "G" and "g" map to 2 or 0b10
+// "T" and "t" map to 3 or 0b11
+// "N" maps to 4 or 0b100.
+// ^ This is POSSIBLE because u2 doesn't exist and we HAVE to use u8 anyway.
+// ^ This should probably change.
+// Everything else maps to 5 or 0b101.
+// ^ This should probably also change.
+static SEQ_NT4_TABLE: [u8; 256] = {
+    let mut table = [5; 256]; // Default 5
+    table[b'N' as usize] = 4;
+
+    table[b'A' as usize] = 0;
+    table[b'C' as usize] = 1;
+    table[b'G' as usize] = 2;
+    table[b'T' as usize] = 3;
+    table[b'a' as usize] = 0;
+    table[b'c' as usize] = 1;
+    table[b'g' as usize] = 2;
+    table[b't' as usize] = 3;
+
+    table
+};
+
+// return u2-encoded nt. See SEQ_NT4_TABLE directly above this function.
 pub fn nt_encoded_as_u2(nt_utf8: &u8) -> u8 {
     SEQ_NT4_TABLE[*nt_utf8 as usize]
 }
 
+// modifies kmer_bits in place.
+// Shift the bit-string 2 bits to the left, slap on the 2-bit encoded nucleotide,
+// and cut off the extra 2 bits
 pub fn pack_nt_onto_kmer(kmer_bits: &mut u64, nt: u8, mask: &u64) {
     *kmer_bits <<= 2; // make room for a new nt with bit-shift-left.
     *kmer_bits |= nt as u64; // turn the first 2 bits into the nt using bitwise OR
     *kmer_bits &= mask; // erase bits over the length of k with bitwise AND
 }
 
-// This turns a sequence of DNA (represented by a u8 slice) into kmers.
+// Turn a sequence of DNA (u8 slice of utf-8 encoded nucleotides) into kmers.
 pub fn seq_to_kmers(sequence: &[u8], kmer_args: &KmerSpecificArgs) -> Result<Vec<SeedObject>> {
     if (kmer_args.k == 0) | (sequence.len() < kmer_args.k) {
         return Ok(Vec::<SeedObject>::new());
@@ -61,7 +91,7 @@ pub fn seq_to_kmers(sequence: &[u8], kmer_args: &KmerSpecificArgs) -> Result<Vec
         let nt = nt_encoded_as_u2(&nt_utf8);
         if nt >= 4 {l = 0;kmer_bits = 0;} else { // restart if nt == 'N'. Idk why, its from sahlin.
             pack_nt_onto_kmer(&mut kmer_bits, nt, &mask);
-            if {l += 1; l >= kmer_args.k} { // kmer_bits represents a kmer!
+            if {l += 1; l >= kmer_args.k} { // kmer_bits IS the kmer identifier!
                 kmers.push(SeedObject {
                     identifier: kmer_bits.clone(),
                     ref_index: kmer_args.ref_index.clone(),
@@ -103,6 +133,7 @@ mod kmer_tests {
     }
     
     #[test]
+    // This test demonstrates that my kmer implementation works.
     fn test_seq_to_kmers_demonstration() -> Result<()>{
         let kmers_returned =seq_to_kmers(
             b"ACTG", 
@@ -149,25 +180,39 @@ mod kmer_tests {
 
 
 // --------------------------------------------------
-// Hashing code -- used for fast index-to-kmer lookups in strobemers
+// Hashing code. This is used in strobemers for minimization.
+// any k-mer has its own unique representation as a bitpacked integer.
+// The issue is that these are ordered, so minimizing a window isn't
+// a straightforward process.
 
 // Takes a u64 (a bit-representation of a seed) which we want to hash.
 // The mask restricts the output hash to 0..= 2*k. I don't really know why.
-pub fn hash64(item: &u64, mask: &u64) -> u64 {
-    let mut key = !item.saturating_add(item << 21) & mask; // key = (key << 21) - key - 1;
-    key = key ^ key >> 24;
-    key = key.saturating_add(key << 3) + (key << 8) & mask; // key * 265
-    key = key ^ key >> 14;
-    key = key.saturating_add(key << 2) + (key << 4) & mask; // key * 21
-    key = key ^ key >> 28;
-    key = key.saturating_add(key << 31) & mask;
+pub fn hash64(u2encoded_seed: &u64, mask: &u64) -> u64 {
+    let mut key = u2encoded_seed.clone();
+    key = (!key).wrapping_add(key.wrapping_shl(21));
+    key &= mask;
+    key = key ^ key.wrapping_shr(24);
+    key = key.wrapping_add(key.wrapping_shl(3)).wrapping_add(key.wrapping_shl(8));
+    key &= mask;
+    key = key ^ key.wrapping_shr(14);
+    key = key.wrapping_add(key.wrapping_shl(2)).wrapping_add(key.wrapping_shl(4));
+    key &= mask;
+    key = key ^ key.wrapping_shr(28);
+    key = key.wrapping_add(key.wrapping_shl(31));
+    key &= mask;
     key
 }
 
-// Lookup by index in the sequence
-pub fn kmer_hash_table(seq: &[u8], k: &usize) -> HashMap<usize, u64> {
-    let mut hash_at_idx = HashMap::new();
-    let mask: u64 = (0b1 << (2 * k)) - 1; // 1 repeated 2*k times
+// maps kmer indices to hash values NOT determined by their u2-encodings
+pub fn kmer_hash_index(seq: &[u8], k: &usize) -> HashMap<usize, u64> {
+    let mut hash_table = HashMap::new();
+    let mask = match k {
+        0 => {return hash_table;},
+        1..=31 => 1_u64.wrapping_shl(*k as u32 * 2) - 1,
+        32 => 0_u64.wrapping_sub(1), // max value for u64
+        _ => {return hash_table;},
+    };
+
     let mut kmer_bits = 0_u64;
     
     let mut l = 0;
@@ -178,16 +223,97 @@ pub fn kmer_hash_table(seq: &[u8], k: &usize) -> HashMap<usize, u64> {
             pack_nt_onto_kmer(&mut kmer_bits, nt, &mask);
             if {l += 1; l >= *k} {
                 let hash = hash64(&kmer_bits, &mask);
-                hash_at_idx.insert(idx + 1 - k, hash); // it will never have an entry
+                hash_table.insert(idx + 1 - k, hash); // it will never have an entry
             }
         }
     }
-    hash_at_idx
+    hash_table
 }
+
+
+// --------------------------------------------------
+// Tests for hashing methods
+#[cfg(test)]
+mod hashing_tests {
+    use std::collections::HashMap;
+    use crate::kmer_hash_index;
+    use crate::hash64;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_hash64_simple() {
+        assert_eq!(0b00000011, hash64(&0b00, &0b00000011)); // A
+        assert_eq!(0b00000011, hash64(&0b00, &0b00001111)); // AA
+        assert_eq!(0b00000011, hash64(&0b00, &0b00111111)); // AAA
+        assert_eq!(0b01000011, hash64(&0b00, &0b11111111)); // AAAA
+
+        assert_eq!(0b00000010, hash64(&0b01, &0b00000011)); // C
+        assert_eq!(0b00000110, hash64(&0b01, &0b00001111)); // CA
+        assert_eq!(0b00000110, hash64(&0b01, &0b00111111)); // CAA
+        assert_eq!(0b10000110, hash64(&0b01, &0b11111111)); // CAAA
+
+        assert_eq!(0b00000001, hash64(&0b10, &0b00000011)); // G
+        assert_eq!(0b00001001, hash64(&0b10, &0b00001111)); // GA
+        assert_eq!(0b00001001, hash64(&0b10, &0b00111111)); // GAA
+        assert_eq!(0b11001001, hash64(&0b10, &0b11111111)); // GAAA
+
+        assert_eq!(0b00000000, hash64(&0b11, &0b00000011)); // T
+        assert_eq!(0b00001100, hash64(&0b11, &0b00001111)); // TA
+        assert_eq!(0b00001100, hash64(&0b11, &0b00111111)); // TAA
+        assert_eq!(0b00001100, hash64(&0b11, &0b11111111)); // TAAA
+    }
+
+    #[test]
+    fn test_hash64_complex() {
+        assert_eq!(0b0100111, hash64(&0b01001100, &0b11111111)); // CATA
+        assert_eq!(0b0010011, hash64(&0b11110000, &0b11111111)); // TTAA
+    }
+
+    #[test]
+    fn test_kmer_hash_table_demonstration(){
+        let hash_table = kmer_hash_index(b"ACGT", &2);
+        let mut true_hash_table = HashMap::new();
+        true_hash_table.insert(0, hash64(&0b0001, &0b1111)); // AC
+        true_hash_table.insert(1, hash64(&0b0110, &0b1111)); // CG
+        true_hash_table.insert(2, hash64(&0b1011, &0b1111)); // GT
+        assert_eq!(hash_table, true_hash_table);
+    }
+    #[test]
+    fn test_kmer_hash_table_seqlen33_k32(){
+        let table = kmer_hash_index(b"AAAACCCCGGGGTTTTAAAACCCCGGGGTTTTA", &32);
+        let mut true_table = HashMap::new();
+        true_table.insert(0, hash64(
+            &0b0000000001010101101010101111111100000000010101011010101011111111, 
+            &0b1111111111111111111111111111111111111111111111111111111111111111)
+        );
+        true_table.insert(1, hash64(
+            &0b0000000101010110101010111111110000000001010101101010101111111100, 
+            &0b1111111111111111111111111111111111111111111111111111111111111111)
+        );
+        assert_eq!(table, true_table);
+    }
+
+    #[test]
+    fn test_kmer_hash_table_k33(){
+        let table = kmer_hash_index(b"AAAACCCCGGGGTTTTAAAACCCCGGGGTTTTA", &33);
+        let true_table = HashMap::new();
+        assert_eq!(table, true_table);
+    }
+
+    #[test]
+    fn test_kmer_hash_table_k0(){
+        let table = kmer_hash_index(b"AAAACCCCGGGGTTTTAAAACCCCGGGGTTTTA", &0);
+        let true_table = HashMap::new();
+        assert_eq!(table, true_table);
+    }
+}
+
 
 // --------------------------------------------------
 // Strobemers code
 
+// We have Minstrobes, Randstrobes and Hybridstrobes.
+// This makes our life easier for pattern matching.
 #[derive(Debug, Clone)]
 pub enum Protocol {
     Rand,
@@ -195,7 +321,7 @@ pub enum Protocol {
     Hybrid,
 }
 
-impl FromStr for Protocol {
+impl FromStr for Protocol { // for pattern matching
     type Err = String;
 
     fn from_str(input: &str) -> Result<Protocol, Self::Err> {
@@ -208,7 +334,7 @@ impl FromStr for Protocol {
     }
 }
 
-impl fmt::Display for Protocol {
+impl fmt::Display for Protocol { // for formatting/displaying to a string.
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let protocol_str = match self {
             Protocol::Rand => "rand",
@@ -220,44 +346,15 @@ impl fmt::Display for Protocol {
 }
 
 
-/// This function generates a string's set of strobemers.
-/// Here's an intuitive breakdown of what strobemers
-/// are and how they're generated:
-///
-/// Stromebers are the concatenation of strobes. There are *order*
-/// strobes in each strobemer.
-/// Each strobemer is drawn from a span of length strobemer_span_length =
-///    strobe_length + (strobe_window_gap + strobe_window_length) * order,
-/// representing the first strobe and all of the remaining strobe windows.
-/// In any sequence, there are
-///    (sequence.len() - strobemer_span_length) / step
-/// strobemers.
-///
-/// The n'th strobemer is drawn from
-///    sequence[n * step..n * step + order * l].
-/// In each strobemer there are *order* strobes.
-/// In each strobemer, the first strobe's index is alwoys at the very
-/// start of the strobemer's span. It is NOT determined by the argmin
-/// of a hash function.
-/// The n'th strobe (not including the first strobe, of
-/// course) is the l-mer minimizer of a window within the strobemer's
-/// span. Specifically, that window is:
-///    window_end = strobe_length + (strobe_window_gap + strobe_window_length) * n
-///    window_end = window_start - strobe_window_length
-///    strobemer_span[window_start..window_end].
-
-/* HELPER FUNCTION FOR strobemer_euclidean_distance()
- * This function generates a string's set of strobemers.
- */
 pub fn seq_to_strobemers(seq: &[u8], args: &StrobemerSpecificArgs) -> Result<Vec<SeedObject>> {
-    let mut seed_vector: Vec<SeedObject> = Vec::new();
+    let mut strobemers: Vec<SeedObject> = Vec::new();
 
     if seq.len() < args.w_max {
-        return Ok(seed_vector);
+        return Ok(strobemers);
     }
 
     // pre-compute hashes for l-mers at every index
-    let lmer_hash_at_index: HashMap<usize, u64> = kmer_hash_table(&seq, &args.strobe_length);
+    let lmer_hash_at_index: HashMap<usize, u64> = kmer_hash_index(&seq, &args.strobe_length);
 
     for strobemer_start_idx in 0..=seq.len() { // One strobemer for each strobe start index
         let mut current_strobemer_hash = lmer_hash_at_index[&strobemer_start_idx];
@@ -274,7 +371,7 @@ pub fn seq_to_strobemers(seq: &[u8], args: &StrobemerSpecificArgs) -> Result<Vec
                   && seq.len() <= end_of_last_window + &args.w_max {
                     end_of_last_window + &args.w_min..seq.len() - 1
                 } else {
-                    return Ok(seed_vector)
+                    return Ok(strobemers)
                 }
             };
 
@@ -291,14 +388,14 @@ pub fn seq_to_strobemers(seq: &[u8], args: &StrobemerSpecificArgs) -> Result<Vec
         }
 
         // construct the SeedObject vector
-        seed_vector.push(SeedObject {
+        strobemers.push(SeedObject {
             identifier: current_strobemer_hash,
             ref_index: args.ref_index.clone(),
             local_index: strobemer_start_idx,
             word_indices: strobe_indices,
         });
     }
-    Ok(seed_vector)
+    Ok(strobemers)
 }
 
 
@@ -319,7 +416,11 @@ pub fn select_randstrobe_index_and_hash(
     Ok((next_strobe_index, next_strobe_hash))
 }
 
-// It's exactly what it sounds like.
+// --------------------------------------------------
+// Similarity methods
+// --------------------------------------------------
+
+
 pub fn jaccard_similarity(base_seed_bag: &Vec<SeedObject>, mod_seed_bag: &Vec<SeedObject>
 ) -> Result<f64> {
 
@@ -342,8 +443,60 @@ pub fn jaccard_similarity(base_seed_bag: &Vec<SeedObject>, mod_seed_bag: &Vec<Se
     Ok(intersection / union)
 }
 
+
+// --------------------------------------------------
+// Tests for similarity methods
+// --------------------------------------------------
+
+#[cfg(test)]
+mod jaccard_similarity_tests {
+    use crate::jaccard_similarity;
+    use crate::SeedObject;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_jaccard_similarity_simple() {
+        let seed_1 = SeedObject{
+            identifier: 0b00,
+            ref_index: 0,
+            local_index: 0,
+            word_indices: vec![]
+        };
+        let seed_1_clone = seed_1.clone();
+        let mut seed_2 = seed_1.clone();
+        seed_2.local_index = 1;
+        let seed_2_clone = seed_2.clone();
+        let mut seed_3 = seed_1.clone();
+        seed_3.local_index = 2;
+        let seed_3_clone = seed_3.clone();
+        let mut seed_bag1 = Vec::<SeedObject>::new();
+        seed_bag1.push(seed_1);
+        let mut seed_bag2 = Vec::<SeedObject>::new();
+        seed_bag2.push(seed_1_clone);
+        assert_eq!(1.0, jaccard_similarity(&seed_bag1, &seed_bag2).unwrap());
+
+        seed_bag1.push(seed_2);
+        assert_eq!(2.0/3.0, jaccard_similarity(&seed_bag1, &seed_bag2).unwrap());
+        seed_bag1.push(seed_3);
+        assert_eq!(2.0/4.0, jaccard_similarity(&seed_bag1, &seed_bag2).unwrap());
+
+        seed_bag2.push(seed_2_clone);
+        seed_bag2.push(seed_3_clone);
+        assert_eq!(1.0, jaccard_similarity(&seed_bag1, &seed_bag2).unwrap());
+
+        
+    }
+}
+
 // --------------------------------------------------
 // Match/NAMs related code.
+pub struct Match<'a> {
+    pub ref_id: &'a str,
+    pub query_id: &'a str,
+    pub ref_idx: usize,
+    pub query_idx: usize
+}
+
 
 #[allow(warnings)]
 pub fn find_nams<'a>(
@@ -376,75 +529,4 @@ pub fn find_nams<'a>(
     }
 
     Ok(nams)
-} 
-
-pub struct Match<'a> {
-    pub ref_id: &'a str,
-    pub query_id: &'a str,
-    pub ref_idx: usize,
-    pub query_idx: usize
-}
-
-static SEQ_NT4_TABLE: [u8; 256] = {
-    let mut table = [5u8; 256]; // Default all values to 5 (error or invalid character)
-
-    table[b'A' as usize] = 0;
-    table[b'C' as usize] = 1;
-    table[b'G' as usize] = 2;
-    table[b'T' as usize] = 3;
-    table[b'a' as usize] = 0;
-    table[b'c' as usize] = 1;
-    table[b'g' as usize] = 2;
-    table[b't' as usize] = 3;
-
-    table[b'N' as usize] = 4;
-
-    table
-};
-
-/* Tests for kmer_hash_table and hash64. */
-#[cfg(test)]
-mod hashing_tests {
-    use std::collections::HashMap;
-    use crate::kmer_hash_table;
-    use crate::hash64;
-    use pretty_assertions::assert_eq;
-
-    #[test]
-    fn test_kmer_hash_table_basic(){
-        let k = 2;
-        let mask = (1 << (2 * k)) - 1;
-        let kmer_hash_at_idx = kmer_hash_table(b"ACGT", &k);
-        let mut my_hash_hap = HashMap::new();
-        my_hash_hap.insert(0, hash64(&0b0001, &mask));
-        my_hash_hap.insert(1, hash64(&0b0110, &mask));
-        my_hash_hap.insert(2, hash64(&0b1011, &mask));
-        assert_eq!(kmer_hash_at_idx, my_hash_hap);
-    }
-
-    #[test]
-    fn test_kmer_hash_table_16(){
-        let k = 16;
-        let mask = (1 << (2 * k)) - 1;
-        let kmer_hash_at_idx = kmer_hash_table(b"AAAAAAAAAAAAAAAA", &k);
-        let mut my_hash_hap = HashMap::new();
-        my_hash_hap.insert(0, hash64(&0b0, &mask));
-        assert_eq!(kmer_hash_at_idx, my_hash_hap);
-    }
-
-    #[test]
-    fn test_kmer_hash_table_17(){
-        let k = 17;
-        let mask = (1 << (2 * k)) - 1;
-        let kmer_hash_at_idx = kmer_hash_table(b"AAAAAAAAAAAAAAAAA", &k);
-        let mut my_hash_hap = HashMap::new();
-        my_hash_hap.insert(0, hash64(&0b0, &mask));
-        assert_eq!(kmer_hash_at_idx, my_hash_hap);
-    }
-
-    #[test]
-    fn test_hash64_basic() {
-        let hash = hash64(&(0b1011 as u64), &(0b1111 as u64));
-        assert_eq!(hash, 0b100);
-    }
 }
