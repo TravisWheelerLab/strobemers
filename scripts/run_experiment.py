c35dcf5ce737e9243acb453a45c361b609d49234
Zach Keyes <56801478+imzoc@users.noreply.github.com>
2024-10-03 06:20:53 -0700

    moved scripts to /scripts from /src/scripting/. QOL.

--- scripts/run_experiment.py
+++ scripts/run_experiment.py
@@ -2,15 +2,13 @@ import sys
 import os
 
 """ I want this script to automate experiments.
-1. Take a folder name.
-2. In the folder, match query files to reference files.
-3. Run strobemer program on pairs of query-reference files.
-4. Combine outputted csv files from rust program into one.
+* Run strobemer_comparison on a bunch of query-reference files.
+* 
 """
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python script.py <directory_path>")
-        sys.exit(1)
+        sys.exit(1) 
     
     directory_path = sys.argv[1]
     assert os.path.isdir(directory_path)
